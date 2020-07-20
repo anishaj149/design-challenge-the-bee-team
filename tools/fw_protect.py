387309624d223ed2b26a1fb1736975a0dbd392f3
RitG8 <ritgurjar@gmail.com>
2020-07-20 19:01:01 +0000

    Finished CBC encryption

--- tools/fw_protect.py
+++ tools/fw_protect.py
@@ -5,7 +5,7 @@ Firmware Bundle-and-Protect Tool
 import argparse
 import struct
 from Crypto.Cipher import AES
-
+from Crypto.Util.Padding import pad
 
 def protect_firmware(infile, outfile, version, message):
     # Load firmware binary from infile
@@ -37,9 +37,29 @@ def protect_firmware(infile, outfile, version, message):
 
 def cbc_encryption(firmware):
     #Firmware format is just the firmware. The size and release message are to be added later. 
-    cipher = AES.new(key, AES.MODE_CBC)
+    #mode is aes-128
+    #How do i read the key from the text file? Which key will it be?
+    #Append IVs to the end of the ciphertext
+    #128 bit IV
+    with open('secret_build_output.txt','rb') as fp:
+        key = fp.readlines()  #Returns a list. Each line is an index in the list.
+        key = key[0]  #key should be 16 bytes long
+        key = key.rstrip()  #removes any possible newlines 
+        
+    cipher = AES.new(key, AES.MODE_CBC)  #makes a cipher object with a random IV
     
+    ciphertext = cipher.encrypt(pad(firmware, AES.block_size)) 
+    #encrypts the firmware and pads it so the firmware length is a multiple of 16 bytes
     
+    ciphertext = bytearray(ciphertext)
+    iv = bytearray(cipher.iv)
+    #makes these into bytearrays so they can be concatenated together
+    
+    final_encrypt = ciphertext + iv  
+    #The final encryption to be returned
+    #The firmware sends the metadata size, so the IV will be easily distinguished
+    
+    return final_encrypt  #returns CBC encryption
     
 def hmac_generation():
     pass
