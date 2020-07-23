0ce730ce879d4127a60684531a5f2a3c03203105
mgite03 <meghnagite@gmail.com>
2020-07-23 19:52:49 +0000

    reorganized protect tool

--- tools/.ipynb_checkpoints/fw_protect-checkpoint.py
+++ tools/.ipynb_checkpoints/fw_protect-checkpoint.py
@@ -5,7 +5,7 @@ Firmware Bundle-and-Protect Tool
 import argparse
 import struct
 
-
+from crypto.Util.Padding import pad
 from Crypto.Hash import HMAC, SHA256
 
 def protect_firmware(infile, outfile, version, message):
@@ -13,25 +13,27 @@ def protect_firmware(infile, outfile, version, message):
     with open(infile, 'rb') as fp:
         firmware = fp.read()
 
-    #encrypts the firmware w cbc mode aes-128
-    encrypted_firmware = cbc_encryption(firmware)
+    #encrypts the firmware w cbc mode aes-128, adds an iv to the end
+    enc_firmware_iv = cbc_encryption(firmware)
     
     # Pack version and size into two little-endian shorts
     metadata = struct.pack('<HH', version, len(firmware))
     
-    
-    #generates an hmac from the unencrypted metadata and the encrypted firmware
-    hmac = hmac_generation(metadata, encrypted_firmware)
-    
-    
-    # Append null-terminated message to end of firmware
-    firmware_and_message = encrypted_firmware + message.encode() + b'\00'
+    # Append null-terminated release message to end of firmware
+    firmware_iv_message = enc_firmware_iv + message.encode() + b'\00'
 
-   
+    firmware_iv_message = pad(firmware_iv_message, 32)
 
-    # Append firmware and message to metadata
-    firmware_blob = metadata + firmware_and_message + hmac
-
+    # Start the firmware blob with the metadata and the metadata's hmac  
+    firmware_blob = metadata + hmac_generation(metadata)
+    
+    
+    #takes 32 bytes of data, generates an hmac, and appends both to firmware_blob  
+    for i in range(0, len(firmware_iv_message), 32)
+        firmware_blob += firmware_iv_message[i, i+32]
+        firmware_blob += hmac_generation(firmware_iv_message[i, i+32])
+    
+    
     # Write firmware blob to outfile
     with open(outfile, 'wb+') as outfile:
         outfile.write(firmware_blob)
@@ -39,12 +41,14 @@ def protect_firmware(infile, outfile, version, message):
 def cbc_encryption():
     pass
 def hmac_generation(metadata, ciphertext):
-#     with open("secret_build_output.txt", "rb") as f:
-#         key_list = f.readlines()
-#     key = key_list[1].rstrip()
+    
+    #reading in a key from the secret file
+    with open("secret_build_output.txt", "rb") as f:
+        key_list = f.readlines()
+    key = key_list[1].rstrip()
 
     
-    key = b'0123456789012345678901234567890123456789012345678901234567890123'
+    
     
     #generates a new hmac object
     h = HMAC.new(key, digestmod=SHA256)
